8c47263cb0e6f444a2aa0eb3910ddcf15a15cc68
nbari <nbari@dalmp.com>
2015-07-23 17:23:00 +0200

    modified:   ../yaml/parse.go

--- yaml/parse.go
+++ yaml/parse.go
@@ -2,29 +2,46 @@ package main
 
 import (
 	"fmt"
-	_ "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	_ "log"
 )
 
-type Version struct {
-	Version string `json:"version" yaml:"version"`
+type Config struct {
+	Versions []string
+	Hosts    []Host
+	Handlers Handler
 }
 
-type Versions []Version
+type Host struct {
+	Host  string
+	Vroot string
+}
 
-type T struct {
-	V []string
-	H struct {
-	}
+type Handler struct {
+	Default []HandlerData
+	Extra   []HandlerData
+}
+
+type HandlerData struct {
+	URL     string
+	Handler string
+	Methods []string
 }
 
 func main() {
-	data, err := ioutil.ReadFile("router.yml")
+
+	// var data map[string]interface{}
+	data := Config{}
+
+	file, err := ioutil.ReadFile("router.yml")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(string(data))
+	if err := yaml.Unmarshal(file, &data); err != nil {
+		panic(err)
+	}
 
+	//fmt.Println(data)
+	fmt.Printf("%+v\n", data)
 }
