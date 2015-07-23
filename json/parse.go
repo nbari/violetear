380e2dc172a2b1b4e8fc65f0fbd17c7ddd136945
nbari <nbari@dalmp.com>
2015-07-23 17:08:35 +0200

    deleted:    handlers.json 	modified:   parse.go

--- json/parse.go
+++ json/parse.go
@@ -6,31 +6,42 @@ import (
 	"io/ioutil"
 )
 
-type H struct {
-	host, vroot string
+type Config struct {
+	Versions []string
+	Hosts    []Host
+	Handlers Handler
 }
 
-type T struct {
-	//	Versions []string
-	//	Hosts    []map[string]string
-	Handlers []map[string]map[string]string
+type Host struct {
+	Host  string
+	Vroot string
 }
 
-// map[versions:[v0 v1 v2] hosts:[map[host:* vroot:default] map[host:*.zunzun.io vroot:default] map[host:ejemplo.org vroot:ejemplo] map[host:api.ejemplo.org vroot:ejemplo]] handlers:map[default:[map[url:/test/.* handler:my_handler methods:[GET POST PUT]] map[url:/(md5|sha1|sha256|sha512)(/.*)? handler:hash_handler methods:[GET]] map[url:/.* handler:default]] ejemplo:[map[url:/.* handler:other_handler methods:[ALL]]]]]
+type Handler struct {
+	Default []HandlerData
+	Extra   []HandlerData
+}
+
+type HandlerData struct {
+	URL     string
+	Handler string
+	Methods []string
+}
 
 func main() {
-	//	var data map[string]interface{}
-	var data T
+
+	// var data map[string]interface{}
+	data := Config{}
+
 	file, err := ioutil.ReadFile("router.json")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	err = json.Unmarshal(file, &data)
-	if err != nil {
-		fmt.Println(data)
+	if err := json.Unmarshal(file, &data); err != nil {
+		panic(err)
 	}
 
-	fmt.Println(data)
-
+	//fmt.Println(data)
+	fmt.Printf("%+v\n", data)
 }
