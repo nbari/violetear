3ceedf70df9417b62f16edae2ac82e215f54f565
nbari <nbari@dalmp.com>
2015-08-26 22:21:44 +0200

    modified:   response_writer.go

--- response_writer.go
+++ response_writer.go
@@ -2,36 +2,37 @@ package violetear
 
 import "net/http"
 
-// LogResponseWritter wraps the standard http.ResponseWritter allowing for more
+// ResponseWritter wraps the standard http.ResponseWritter allowing for more
 // verbose logging
-type ResponseWritter struct {
+type ResponseWriter struct {
 	http.ResponseWriter
 	status int
 	size   int
 }
 
-func NewResponseWritter(w http.ResponseWriter) *ResponseWritter {
-	return &ResponseWritter{w, 0, 0}
+// NewResponseWriter returns ResponseWriter
+func NewResponseWriter(w http.ResponseWriter) *ResponseWriter {
+	return &ResponseWriter{w, 0, 0}
 }
 
 // Status provides an easy way to retrieve the status code
-func (w *ResponseWritter) Status() int {
+func (w *ResponseWriter) Status() int {
 	return w.status
 }
 
 // Size provides an easy way to retrieve the response size in bytes
-func (w *ResponseWritter) Size() int {
+func (w *ResponseWriter) Size() int {
 	return w.size
 }
 
 // Header returns & satisfies the http.ResponseWriter interface
-func (w *ResponseWritter) Header() http.Header {
+func (w *ResponseWriter) Header() http.Header {
 	return w.ResponseWriter.Header()
 }
 
 // Write satisfies the http.ResponseWriter interface and
 // captures data written, in bytes
-func (w *ResponseWritter) Write(data []byte) (int, error) {
+func (w *ResponseWriter) Write(data []byte) (int, error) {
 	if w.status == 0 {
 		w.WriteHeader(http.StatusOK)
 	}
@@ -44,8 +45,7 @@ func (w *ResponseWritter) Write(data []byte) (int, error) {
 
 // WriteHeader satisfies the http.ResponseWriter interface and
 // allows us to cach the status code
-func (w *ResponseWritter) WriteHeader(statusCode int) {
-
+func (w *ResponseWriter) WriteHeader(statusCode int) {
 	w.status = statusCode
 	w.ResponseWriter.WriteHeader(statusCode)
 }
