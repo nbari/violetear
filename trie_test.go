5817878a52a91c7cbc3ae9a1613ee9f307fcbf32
nbari <nbari@dalmp.com>
2015-08-27 21:38:10 +0200

    modified:   trie_test.go

--- trie_test.go
+++ trie_test.go
@@ -1,7 +1,6 @@
 package violetear
 
 import (
-	"fmt"
 	"net/http"
 	"testing"
 )
@@ -89,12 +88,11 @@ func TestTrieGet(t *testing.T) {
 	err = trie.Set([]string{"root", "alpha1", "*"}, nil, "ALL")
 	expect(t, err, nil)
 
-	err = trie.Set([]string{"root", "alpha2", "*"}, nil, "ALL")
+	err = trie.Set([]string{"root", "alpha2", ":dynamic"}, nil, "ALL")
 	expect(t, err, nil)
 
 	err = trie.Set([]string{"root", "alpha", "beta", "gamma"}, nil, "ALL")
 	expect(t, err, nil)
-
 	err = trie.Set([]string{"alpha", "*"}, nil, "ALL")
 	expect(t, err, nil)
 
@@ -105,6 +103,90 @@ func TestTrieGet(t *testing.T) {
 
 	n, p, l, err := trie.Get([]string{"*"})
 	expect(t, err, nil)
-	fmt.Println(n, p, l)
+	expectDeepEqual(t, p, []string{"*"})
+	expect(t, l, true)
 
+	n, p, l, err = trie.Get([]string{"not_found"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"not_found"})
+	expect(t, l, false)
+	expect(t, n.HasRegex, true)
+
+	n, p, l, err = trie.Get([]string{":dynamic"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{":dynamic"})
+	expect(t, l, true)
+
+	n, p, l, err = trie.Get([]string{"root"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"root"})
+	expect(t, l, true)
+	expect(t, len(n.Node), 4)
+
+	n, p, l, err = trie.Get([]string{"root", "alpha"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"alpha"})
+	expect(t, l, true)
+	expect(t, len(n.Node), 2)
+
+	n, p, l, err = trie.Get([]string{"root", "not_found"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"not_found"})
+	expect(t, l, false)
+	expect(t, len(n.Node), 4)
+
+	n, p, l, err = trie.Get([]string{"root", "alpha1"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"alpha1"})
+	expect(t, l, true)
+	expect(t, len(n.Node), 1)
+
+	n, p, l, err = trie.Get([]string{"root", "alpha1", "any"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"any"})
+	expect(t, l, false)
+	expect(t, len(n.Node), 1)
+	expect(t, n.HasRegex, false)
+
+	n, p, l, err = trie.Get([]string{"root", "alpha2", "any"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"any"})
+	expect(t, l, false)
+	expect(t, len(n.Node), 1)
+	expect(t, n.HasRegex, true)
+
+	n, p, l, err = trie.Get([]string{"root", "alpha", "beta"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"beta"})
+	expect(t, l, true)
+	expect(t, len(n.Node), 1)
+	expect(t, n.HasRegex, false)
+
+	n, p, l, err = trie.Get([]string{"root", "alpha", "beta", "gamma"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"gamma"})
+	expect(t, l, true)
+	expect(t, len(n.Node), 0)
+	expect(t, n.HasRegex, false)
+
+	n, p, l, err = trie.Get([]string{"root", "alphaA", "betaB", "gammaC"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"alphaA", "betaB", "gammaC"})
+	expect(t, l, false)
+	expect(t, len(n.Node), 4)
+	expect(t, n.HasRegex, false)
+
+	n, p, l, err = trie.Get([]string{"root", "alpha", "betaB", "gammaC"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"betaB", "gammaC"})
+	expect(t, l, false)
+	expect(t, len(n.Node), 2)
+	expect(t, n.HasRegex, false)
+
+	n, p, l, err = trie.Get([]string{"root", "alpha", "betaB", "gamma", "delta"})
+	expect(t, err, nil)
+	expectDeepEqual(t, p, []string{"betaB", "gamma", "delta"})
+	expect(t, l, false)
+	expect(t, len(n.Node), 2)
+	expect(t, n.HasRegex, false)
 }
