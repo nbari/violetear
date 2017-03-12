34b54d7ec9bb6cc430816bf2bd81c4c8df8fd927
nbari <nbari@tequila.io>
2017-03-12 18:23:20 +0100

    errorHandler prototype

---
+++ errorHandler.go
@@ -0,0 +1,46 @@
+package violetear
+
+import "net/http"
+
+type violetearError interface {
+	error
+	Status() int
+}
+
+// StatusError represents an error with an associated HTTP status code.
+type Error struct {
+	Code int
+	Err  error
+}
+
+// Error return error message
+func (e Error) Error() string {
+	return e.Err.Error()
+}
+
+// Status return  HTTP status code.
+func (e Error) Status() int {
+	return e.Code
+}
+
+// ErrorHandler struct that returns error
+type ErrorHandler func(w http.ResponseWriter, r *http.Request) error
+
+// ServeHTTP allows ErrorHandler type to satisfy http.Handler.
+func (h ErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := h(w, r); err != nil {
+		switch e := err.(type) {
+		case violetearError:
+			// if logErrors  {
+			//	log.Printf("HTTP %d - %s", e.Status(), e)
+			//}
+			http.Error(w, e.Error(), e.Status())
+		default:
+			http.Error(
+				w,
+				http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError,
+			)
+		}
+	}
+}
