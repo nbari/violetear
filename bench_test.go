a16f80c8b7638a8c1f61483f3932f02c1448e4c4
nbari <nbari@tequila.io>
2017-09-23 21:16:28 +0200

    benchmark for methods GET/HEAD

--- bench_test.go
+++ bench_test.go
@@ -25,7 +25,7 @@ func benchRequest(b *testing.B, router http.Handler, r *http.Request) {
 
 func BenchmarkRouter(b *testing.B) {
 	router := New()
-	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {})
+	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {}, "GET,HEAD")
 	r, _ := http.NewRequest("GET", "/hello", nil)
 	benchRequest(b, router, r)
 }
