9b54dc8a2dadf578cd0527790911c6c0dc47b121
nbari <nbari@tequila.io>
2019-03-31 19:37:52 +0200

    Removed fixed regex: "^%s$"

--- dynamic.go
+++ dynamic.go
@@ -2,7 +2,6 @@ package violetear
 
 import (
 	"errors"
-	"fmt"
 	"regexp"
 	"strings"
 )
@@ -12,11 +11,6 @@ type dynamicSet map[string]*regexp.Regexp
 func (d dynamicSet) Set(name, regex string) error {
 	if !strings.HasPrefix(name, ":") {
 		return errors.New("dynamic route name must start with a colon ':'")
-	}
-
-	// fix regex
-	if !strings.HasPrefix(regex, "^") {
-		regex = fmt.Sprintf("^%s$", regex)
 	}
 
 	r := regexp.MustCompile(regex)
